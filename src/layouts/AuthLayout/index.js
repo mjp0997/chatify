93cb98f95b88b609cba69f209be2b5381e47db4d
Mario José Pineda <72577859+mjp0997@users.noreply.github.com>
2023-07-19 00:08:27 -0400

    Layout for auth pages

---
+++ src/layouts/AuthLayout/index.js
@@ -0,0 +1,18 @@
+
+
+
+// Styles
+import { Background } from './styles.js';
+
+
+
+const AuthLayout = ({children}) => {
+
+   return (
+      <Background>{children}</Background>
+   );
+}
+
+
+
+export default AuthLayout;
---
+++ src/layouts/AuthLayout/styles.js
@@ -0,0 +1,15 @@
+import { styled } from 'styled-components';
+
+
+
+export const Background = styled.div`
+   background-color: var(--light-secondary-color);
+   background-image: url('/auth-background.svg');
+   
+   width: 100%;
+   height: 100vh;
+
+   display: flex;
+   align-items: center;
+   justify-content: center;
+`;

be1414a067436ffbc8023ce531d5ea1492313e31
Mario José Pineda <72577859+mjp0997@users.noreply.github.com>
2023-07-19 00:07:32 -0400

    Initializing project

---
+++ src/pages/_app.js
@@ -0,0 +1,18 @@
+
+
+
+// Styles
+import '../styles/index.css';
+
+
+
+const MyApp = ({ Component, pageProps }) => {
+   // Use the layout defined at the page level, if available
+   const getLayout = Component.getLayout || ((page) => page);
+   
+   return getLayout(<Component {...pageProps} />);
+}
+
+
+
+export default MyApp;
